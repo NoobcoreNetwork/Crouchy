dd3ca8b9a184d064a56cf3b0ee725606cef3479c
Josh <admin@lipsmoker.com>
2023-02-17 22:20:16 +0000

    Initial Push!

--- src/main/java/docs/noobcore/com/crouchy/CrouchHandler.java
+++ src/main/java/docs/noobcore/com/crouchy/CrouchHandler.java
@@ -1,6 +1,5 @@
 package docs.noobcore.com.crouchy;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -8,31 +7,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
-
-import java.lang.reflect.GenericDeclaration;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class CrouchHandler implements Listener {
     @EventHandler
     public void onPlayerMoveEvent(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        Block block = player.getLocation().add(0, -1, 0).getBlock().getRelative(BlockFace.UP);
-
+        Block block = player.getLocation().add(0.0, -1.0, 0.0).getBlock().getRelative(BlockFace.UP);
         if (!player.isSneaking()) {
             System.out.println("You stopped sneaking! youre now visible");
-            return;
-        }
-
-        if (block.getType() == Material.TALL_GRASS) {
-            System.out.println("You are sneaking in tall grass, you are invisible");
-            System.out.println("will make invisible here");
-        }
-            else
-            System.out.println("you walked out of grass while sneaking, sneaky boi, you're visible");
-
-
+            player.removePotionEffect(PotionEffectType.INVISIBILITY);
+        } else {
+            if (block.getType() == Material.TALL_GRASS) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 999, 2, false));
+            } else {
+                player.removePotionEffect(PotionEffectType.INVISIBILITY);
             }
+
         }
+    }
+}
+
+
 
 
 
